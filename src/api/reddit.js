179d64564ce8927331c1f55f3289ffdf93c7a98b
chrisleader <chrisleader87@gmail.com>
2023-01-07 21:11:14 +0800

    Updated reddit.js and test

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -1,19 +1,19 @@
-//import fetch from 'node-fetch';
-import { json } from 'react-router-dom';
-
 const baseUrl = 'https://www.reddit.com';
 
-export const getPosts = async (searchTerm) => {
-    const searchSyntax = '/search/?q=';
+const getPosts = async (searchTerm) => {
+    const searchSyntax = '/search.json?q=';
     const urlToFetch = `${baseUrl}${searchSyntax}${searchTerm}`;
 
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
-            const jsonResponse = response.json();
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
             return jsonResponse;
         }
     } catch(error) {
         console.log(error);
     }
 }
+
+export { getPosts };
