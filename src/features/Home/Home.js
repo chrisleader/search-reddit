19aeb834a35ccef2f119c5614d9a6569f42e52b8
chrisleader <chrisleader87@gmail.com>
2023-01-07 22:02:16 +0800

    Renamed Results to Posts; Updated Home

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { ReactComponent as Logo } from './logo.svg'
 import './Home.css';
+import { getPosts } from "../../api/reddit";
+import { useState, useEffect } from "react";
+import Posts from "../Posts/Posts";
 
 const Home = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [results, setResults] = useState('');
+
+    const onChange = ({target}) => {
+        setSearchQuery(target);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setResults(getPosts(searchQuery));
+      }
+
     return (
         <div>
             <Logo className="logo"/>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
-                placeholder="search">
-
-                </input>
-                <button>
+                    type="text"
+                    placeholder="search"
+                    onChange={onChange}
+                />
+                <button
+                    type="submit">
 
                 </button>
             </form>
+            <Posts results={results}/>
         </div>
     )
 }
