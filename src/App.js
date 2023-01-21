ba926feed540548a8273ef21c1a1628594c61b0a
chrisleader <chrisleader87@gmail.com>
2023-01-21 23:22:35 +0800

    Refreshing page maintains state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { getPosts } from './api/reddit';
 import { useState } from 'react';
 import SearchForm from './features/SearchForm/SearchForm';
@@ -14,23 +14,22 @@ function App() {
     setQuery(target.value);
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    getPosts(query)
-      .then(response => {
-        setResults(response);
-        navigate('/results');
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const response = await getPosts(query);
+      setResults(response);
+      navigate(`results/${query}`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<SearchForm query={query} onChange={onChange} onSubmit={onSubmit} />} />
-        <Route path="/results" element={<Results results={results}/>} />
+        <Route path="/results/:term" element={<Results results={results} setResults={setResults} />} />
       </Routes>
     </div>
   );
--- src/features/Results/Results.js
+++ src/features/Results/Results.js
@@ -1,29 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { getPosts } from "../../api/reddit";
 
-const Results = ({results}) => {
+const Results = ({results, setResults}) => {
     const thumbnailExceptions = ['self', 'default', 'nsfw', 'spoiler'];
+    const { term } = useParams();
 
-    if (!results.length) {
-        return (
-            <div>
-            </div>
-        )
-    }
+    useEffect(() => {
+      (async () => {
+        if (term) {
+          const response = await getPosts(term);
+          setResults(response);
+        }
+      })();
+    }, [term]);
     
-    else {
-        return (
-            <div>
-                {results.map((item, index) => (
-                <div key={index}>
-                    <p>
-                        <a href={item.data.url} target='_blank' rel='noreferrer noopener'>{item.data.title}</a>
-                    </p>
-                    {thumbnailExceptions.indexOf(item.data.thumbnail) === -1 && <img src={item.data.thumbnail} alt={item.data.title} />}
-                </div>
-            ))}
+    return (
+        <div>
+            {results.map((item, index) => (
+            <div key={index}>
+                <p>
+                    <a href={item.data.url} target='_blank' rel='noreferrer noopener'>{item.data.title}</a>
+                </p>
+                {thumbnailExceptions.indexOf(item.data.thumbnail) === -1 && <img src={item.data.thumbnail} alt={item.data.title} />}
             </div>
-        )
-    }
+        ))}
+        </div>
+    )
 }
 
 export default Results;
