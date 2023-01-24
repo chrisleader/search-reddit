aa4517d810f64e15e871656156371c3b9d518a9a
chrisleader <chrisleader87@gmail.com>
2023-01-24 23:10:07 +0800

    Further updates to time function and Results CSS

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -4,6 +4,8 @@ const time = (created_utc) => {
     let calculatedTime = Math.floor((secondsSinceEpoch - created_utc));
     const hour = 3600;
     const day = 86400;
+    const month = day * 28;
+    const year = 31556926;
 
     if (calculatedTime/hour <= 1) {
         return '1 hour';
@@ -11,8 +13,16 @@ const time = (created_utc) => {
         return Math.round((calculatedTime/hour).toString()) + ' hours';
     } else if (calculatedTime/day === 1) {
         return '1 day';
-    } else if (calculatedTime/day >= 1) {
+    } else if (calculatedTime/day >= 1 && calculatedTime/day < 29) {
         return Math.round((calculatedTime/day).toString()) + ' days';
+    } else if (calculatedTime/day >= 28 && calculatedTime/day < 56) {
+        return '1 month';
+    } else if (calculatedTime/day >= 56 && calculatedTime/day < 365) {
+        return Math.round((calculatedTime/month).toString()) + ' months';
+    } else if (calculatedTime/day >= 365 && calculatedTime/day < 730) {
+        return '1 year';
+    } else if (calculatedTime/day >= 730) {
+        return Math.round((calculatedTime/year).toString()) + ' years';
     }
 }
 
