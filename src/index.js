1e0a916dc5a83ccdeffab618427072fdbe35042c
chrisleader <chrisleader87@gmail.com>
2023-01-27 17:15:44 +0800

    Testing switching back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
